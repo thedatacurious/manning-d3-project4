0b513e98eb1de2a081528cd7b6706166484da8e7
Alexandra <thedatacurious@gmail.com>
2021-08-18 17:40:50 +0800

    added visual marks for space flight hours per astronaut

--- js/main.js
+++ js/main.js
@@ -5,16 +5,17 @@ const innerCircleRadius = 400; // Inner radius of the visualization (where the g
 const outerCircleRadius = 410; // Outer radius of the visualization (including the flight time lines)
 
 let groups = [];
+let domain_space_flight_total_hours = [];
 
 // Set chart dimensions
 let dimensions = {
   width: 1450,
   height: 1300,
   margin: {
-    top: 100,
-    right: 150,
+    top: 150,
+    right: 100,
     bottom: 0,
-    left: 80,
+    left: 200,
   },
 }
 
@@ -26,7 +27,8 @@ async function radialViz(){
   // Load data
 
   const dataset = await d3.csv('./data/astronauts_nasa_1959-2017.csv').then(d =>
-    {
+    {domain_space_flight_total_hours = d3.extent(d.map(d => +d.space_flight_total_hours))
+
      const aggreg = d3.flatGroup(d, grp => +grp.group, grp => +grp.year, grp => grp.group_name)
      aggreg.forEach(elem =>
        { const group = new Object();
@@ -37,6 +39,8 @@ async function radialViz(){
          groups.push(group);
        }
     )
+
+    // return aggreg;
   })
 
   const wrapper = d3.select('div#viz')
@@ -52,6 +56,7 @@ async function radialViz(){
 
   const cirLayout = d3.pie()
   .value(d => d.astronauts.length)
+  .padAngle(0.01)
   .sort(null);
 
   const groupFormatted = cirLayout(groups)
@@ -63,27 +68,23 @@ async function radialViz(){
       .outerRadius(outerCircleRadius)
       .startAngle(d => d.startAngle)
       .endAngle(d => d.endAngle)
-      .padAngle(0.01)
       .cornerRadius(16);
-
-  console.log(groupFormatted)
 
   // Draw arc
 
   bounds.selectAll('g')
   .data(groupFormatted)
   .join('g')
-  .attr('class', d => 'group-' + d.data.group)
+  .attr('class', d => 'group group-' + d.data.group)
   .append('path')
   .attr('d', arcGen)
   .attr('id', d => 'id-' + d.data.group)
   .style('fill', '#6794AD')
 
-
-bounds.selectAll(".arcLabel")
+  bounds.selectAll('.group')
     .data(groupFormatted)
-    .join("text")
-    .attr("class", "arcLabel")
+    .join('.group')
+    .append("text")
     .attr('dy', d => d.endAngle > 2 && d.endAngle < 5 ? -8 : 30)
     .append("textPath")
     .attr('startOffset', d => d.endAngle > 2 && d.endAngle < 5 ? '75%' : '25%')
@@ -101,6 +102,51 @@ bounds.selectAll(".arcLabel")
   // .attr('y', d=> arcGen.innerRadius(outerCircleRadius-90).centroid(d)[1])
   // .attr('text-anchor','middle')
 
+  // Create scales
+  const scale_space_flight_total_hours = d3.scaleLinear().domain(domain_space_flight_total_hours).range([0,300])
+
+  groupFormatted.forEach((elem) => {
+
+  let partition = (elem.endAngle-elem.startAngle-2*0.01)/elem.value;
+
+  let point = d3.arc()
+        .innerRadius(outerCircleRadius)
+        .outerRadius(outerCircleRadius)
+
+  console.log(elem)
+
+  bounds.select(`.group-${elem.data.group}`)
+  .selectAll('line')
+  .data(elem.data.astronauts)
+  .join('line')
+  .attr('x1', (d,i) =>
+   point.startAngle(elem.startAngle+0.01+partition*i)
+           .endAngle(elem.startAngle+0.01+partition*(i+1))
+           .centroid(d)[0]
+)
+  .attr('y1', (d,i) =>
+   point.startAngle(elem.startAngle+0.01+partition * i)
+           .endAngle(elem.startAngle+0.01+partition*(i+1))
+           .centroid(d)[1]
+)
+.attr('x2', (d,i) =>
+  point.innerRadius(scale_space_flight_total_hours(+d.space_flight_total_hours) + outerCircleRadius)
+           .outerRadius(scale_space_flight_total_hours(+d.space_flight_total_hours) + outerCircleRadius)
+           .startAngle(elem.startAngle+0.01+partition* i)
+           .endAngle(elem.startAngle+0.01+partition*(i+1))
+           .centroid(d)[0]
+)
+.attr('y2', (d,i) =>
+  point.innerRadius(scale_space_flight_total_hours(+d.space_flight_total_hours) + outerCircleRadius)
+           .outerRadius(scale_space_flight_total_hours(+d.space_flight_total_hours) + outerCircleRadius)
+           .startAngle(elem.startAngle+0.01+partition * i)
+           .endAngle(elem.startAngle+0.01+partition*(i+1))
+           .centroid(d)[1]
+)
+.style('stroke', d => d.military_force.length > 0 ? '#C2A83E' : '#718493')
+
+
+})
 
 
 
